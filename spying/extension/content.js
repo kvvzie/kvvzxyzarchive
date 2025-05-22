0e2fa92025bd5dae030950353325f685d2b78543
kvvzie <161234221+kvvzie@users.noreply.github.com>
2025-05-22 23:38:24 +0200

    Delete spying directory

--- spying/extension/content.js
+++
@@ -1,6 +0,0 @@
-setInterval(() => {
-    const videoId = new URLSearchParams(window.location.search).get('v');
-    if (videoId) {
-      chrome.storage.local.set({ currentVideoId: videoId });
-    }
-  }, 5000); // Sprawdza co 5 sekund
